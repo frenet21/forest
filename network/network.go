4f348cef187ee6eadc2e866bd2a69c37af5cba20
kenellorando <kenellorando@gmail.com>
2018-02-10 14:35:10 -0600

    Add empty acceptBlock function

--- network/network.go
+++ network/network.go
@@ -10,6 +10,7 @@ const (
 	LOCAL_SERV_PORT = ":50123"
 )
 
+// Start the server with the given address and port
 func startServer(){
 	ln, err := net.Listen("tcp", LOCAL_SERV_PORT)
 	if err != nil{
@@ -18,8 +19,19 @@ func startServer(){
 		log.Print("Server started on" + LOCAL_SERV_ADDR + LOCAL_SERV_PORT)
 	}
 
-	for{
+	for {
 		conn, _ := ln.Accept()
-		go listen(conn)
+		go acceptBlock(conn)
 	}
+}
+
+// Destring block data, check block ID against known block list
+// Decide to accept or discard
+func acceptBlock(conn net.Conn) {
+	// Destringify the incoming data using function from block package
+	// blockData := block.DestringifyBlockData(conn)
+	/*
+	 TODO: implement check incoming blocks
+	and decide whether they should be dropped or accepted
+	*/
 }
