11acb63fafb59f205019fba8de3848e6feb49865
Eve-GUO <zguo19hawk@gmail.com>
2018-02-10 12:54:59 -0600

    Start server function

--- network.go
+++ network.go
@@ -9,3 +9,16 @@ const (
 	LOCAL_SERV_ADDR = "localhost"
 	LOCAL_SERV_PORT = ":50123"
 )
+func startServer(){
+	ln, err := net.Listen("tcp", LOCAL_SERV_PORT)
+	if err != nil{
+		log.Print("Failed to connect the server.")
+	} else {
+		log.Print("Server started on" + LOCAL_SERV_ADDR + LOCAL_SERV_PORT)
+	}
+
+	for{
+		conn, _ := ln.Accept()
+		go listen(conn)
+	}
+}
