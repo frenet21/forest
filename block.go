c1fd24286c4ae59eb1cba1974d878fed562770f1
Ryan Hodin <ryan951357@gmail.com>
2018-02-10 13:05:42 -0600

    Start implementing blockdata creation

--- block.go
+++ block.go
@@ -1,10 +1,15 @@
 package block
 
-/* import "golang.org/x/crypto/sha3"
+import (
+	"encoding/base64"
+	"io"
+
+	"golang.org/x/crypto/sha3"
+)
 import "crypto/cipher"
 import "crypto/rsa"
 import "crypto/aes"
-import "crypto/rand" */
+import "crypto/rand"
 
 type BlockData struct {
 	encryptedKey     string   // RSA encrypted AES key
@@ -20,4 +25,41 @@ type block struct {
 	// Non-hashed data
 	ID     [64]byte // Hash of block data
 	pepper [8]byte  // Random non-hashed salt
+}
+
+// Returns n random bytes
+func RandomBytes(n int) ([]byte, error) {
+	out := make([]byte, n)
+	return rand.Read(out)
+}
+
+func (message string, parent [64]byte, key rsa.PublicKey) CreateBlockData() BlockData {
+	out := BlockData
+
+	// Block salt
+	out.salt = RandomBytes(8)
+
+	// Random AES256 key
+	AESkey := RandomBytes(32)
+	// Block cipher for that key
+	AESCipher := aes.NewCipher(AESkey)
+	// encrypted message bytes
+	cipherBytes := make([]bytes, aes.BlockSize+len(message))
+	// Initialization Vector
+	// Delivered with ciphertext as it is necessary for decryption...
+	// But it doesn't have to be private to be secure
+	iv := cipherBytes[:aes.BlockSize]
+	// If error reading IV
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err) // panic!!
+	}
+	// Stream cipher
+	stream := cipher.NewCTR(AESCipher, iv)
+	// Plaintext bytes
+	plaintext := []byte(message)
+	// Encryption
+	stream.XORKeyStream(cipherBytes[aes.BlockSize:], plaintext)
+	// Convert to base64 and place in block
+	out.encryptedMessage = base64.URLEncoding.EncodeToString(cipherBytes)
+
 }
