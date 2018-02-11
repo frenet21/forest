14a494d424c51c0e068e16272afdf276f1acc3e4
Ryan Hodin <ryan951357@gmail.com>
2018-02-10 23:18:39 -0600

    Implement genesisPool
    And fix some syntax errors while we're at it

--- pool/pool.go
+++ pool/pool.go
@@ -1,11 +1,11 @@
 package pool
 
 import (
-	"time"
-	"golang.org/x/crypto/sha3"
-	"fmt"
 	"sort"
 	"strings"
+	"time"
+
+	"golang.org/x/crypto/sha3"
 )
 
 type HashDate struct {
@@ -15,16 +15,30 @@ type HashDate struct {
 
 type Blockpool struct {
 	hashes [1000]string
-	queue []HashDate
+	queue  []HashDate
 }
 
 var blockpool Blockpool // Blockpool singleton, usually(?)
 
+func genesisPool() Blockpool {
+	var genesis Blockpool
+
+	hasher := sha3.New512()
+
+	for i := 0; i < len(genesis.hashes); i++ {
+		block := make([]byte, 1)
+		block[0] = byte(i)
+		genesis.hashes[i] = string(hasher.Sum(block)[:64])
+	}
+
+	return genesis
+}
+
 // Selects a block parent based on the encrypted message
-func selectParentHash(string encryptedMessage, pool Blockpool) string {
+func selectParentHash(encryptedMessage string, pool Blockpool) string {
 	//Add hash of encrypted message to the end of the blockpool array
-	hash := string(sha3.New512().Sum(byte[](info.encryptedMessage))[:64])
-	blockpoolStrings := append(pool.hashes, hash)
+	hash := string(sha3.New512().Sum([]byte(encryptedMessage))[:64])
+	blockpoolStrings := append(pool.hashes[:], hash)
 
 	//Sorted blockpool strings
 	sort.Strings(blockpoolStrings)
@@ -33,8 +47,8 @@ func selectParentHash(string encryptedMessage, pool Blockpool) string {
 
 	//Determine the element after the encrypted message
 	for i := 0; i < len(blockpoolStrings); i++ {
-		if(strings.Compare(blockpoolStrings[i], hash) == 0){
-			if(i == len(blockpoolStrings) - 1){
+		if strings.Compare(blockpoolStrings[i], hash) == 0 {
+			if i == len(blockpoolStrings)-1 {
 				element = i + 1
 			}
 		}
@@ -45,5 +59,5 @@ func selectParentHash(string encryptedMessage, pool Blockpool) string {
 }
 
 func main() {
-	
+
 }
