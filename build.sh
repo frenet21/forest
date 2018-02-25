620b1e18c9cb112e3b53bac2a5a37e561d32ec49
kenellorando <kenellorando@gmail.com>
2018-02-24 20:04:36 -0600

    Add printed comments to build script

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Remove the databases
+echo "Deleting databases..."
 echo `rm -rf pubKeys`
 
 # Remove the binary and rebuild
+echo "Deleting binaries..."
 echo `rm -rf ./forest`
+
+echo "Building forest..."
 echo `go build -o forest`
 
 echo "Build script completed."
