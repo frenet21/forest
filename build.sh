071c3b087de9c5ba315afb698b104e201680bd93
kenellorando <kenellorando@gmail.com>
2018-02-24 20:44:18 -0600

    Add go get for dependencies

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ echo "Deleting databases..."
 echo `rm -rf pubKeys`
 
 # Remove the binary and rebuild
-echo "Deleting binaries..."
+echo "Deleting existing binaries..."
 echo `rm -rf ./forest`
+
+echo "Getting external packages..."
+echo `go get golang.org/x/crypto/sha3`
+echo `go get github.com/skratchdot/open-golang/open`
+echo `go get github.com/syndtr/goleveldb/leveldb`
 
 echo "Building forest..."
 echo `go build -o forest`
